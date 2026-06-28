8c3e10f6082fd8f361a9023f5981ee70e159802b
Mathieu Chambaud <mathchambaud2000@gmail.com>
2026-06-28 18:05:48 +0200

    fix: keep onboarding modules after source move

---
+++ apps/marketing/src/components/onboarding/pro/informations-step.tsx
@@ -0,0 +1,13 @@
+import InformationsForm from "../components/pro/informations-form";
+
+const ProInformationsStep = ({
+  nextStep,
+  previousStep,
+}: {
+  nextStep: () => void;
+  previousStep: () => void;
+}) => {
+  return <InformationsForm nextStep={nextStep} previousStep={previousStep} />;
+};
+
+export default ProInformationsStep;
